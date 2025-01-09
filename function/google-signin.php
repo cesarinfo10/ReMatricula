eca58f436a3cb798b65acac0cb0b2551d541d927
César Sánchez <cesarinfo10@gmail.com>
2025-01-09 06:57:20 -0300

    se modifica login

--- function/google-signin.php
+++ function/google-signin.php
@@ -1,5 +1,6 @@
 <?php
 require_once '../vendor/autoload.php';
+include('conexion.php');
 
 $client = new Google_Client(['client_id' => '702303678845-g6o0hnvkosrrhsapc0qnj944ujgl9cuu.apps.googleusercontent.com']);  // Reemplaza YOUR_CLIENT_ID con tu ID de cliente de Google
 $token = json_decode(file_get_contents('php://input'))->token;
@@ -8,18 +9,22 @@ try {
     $payload = $client->verifyIdToken($token);
     if ($payload) {
         
-        $userid = $payload['sub'];
-        $email = $payload['email'];
-        $family_name = $payload['family_name'];
-        $given_name = $payload['given_name'];
+        $email = 'isaachuaiquil.tts@gmail.com';//
+       // $email= $payload['email'];
+;
+        $dbconn = db_connect();
 
-        echo json_encode([
-            'success' => true,
-            'userid' => $userid,
-            'email' => $email,
-            'family_name' => $family_name,
-            'given_name' => $given_name
-        ]);
+        header('Content-type:application/json');
+      
+        $cod_jornada = $_GET['cod_jornada'];
+        $cod_carrera = $_GET['cod_carrera'];
+      
+        $query = "SELECT email FROM public.alumnos where email = '".$email."'";
+        $result = pg_query($query) or die('La consulta fallo: ' . pg_last_error());
+        $row = pg_fetch_row($result);
+        
+       
+        echo json_encode($row);
     } else {
         // Token inválido
         echo json_encode(['success' => false]);
